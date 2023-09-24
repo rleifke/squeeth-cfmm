901a6ba64426ac74d12646fa9cdfd022e8703791
Robert Leifke <r.leifke@gmail.com>
2023-09-24 17:41:29 -0400

    remappings

--- src/Squeeth.sol
+++ src/Squeeth.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.13;
 
 import {BaseHook} from "periphery-next/BaseHook.sol";
-import {ERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
